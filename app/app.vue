c1e138e60c8910e3ad5b12fad802ff5a2507e4e0
rmrmr <t2g7fcuu@yahoo.co.jp>
2026-04-27 00:51:20 +0900

    app/pages/index.vue追加、app/app.vue修正、npx nuxi generate、.output/publicの中身を/hogeに複製

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
+    <!-- ページが切り替わったことをスクリーンリーダーに伝える機能（残しておいてOK） -->
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- pages/ フォルダの中身を表示するためのコンポーネント -->
+    <NuxtPage />
   </div>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>こんにちは！Nuxtの世界へようこそ</h1>
+    <p>READMEではなく、この文字が表示されていれば成功です。</p>
+  </div>
+</template>
